9d3e72a01e455599a88a57b9464d0a6fa8276721
Toran Sahu <toran.sahu@yahoo.com>
2021-02-11 23:24:28 +0530

    implement ServerStreamingExecute() RPC to support 'FIB' operation - server-side streaming

--- server/machine.go
+++ server/machine.go
@@ -10,8 +10,10 @@ package server
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/toransahu/grpc-eg-go/machine"
+	"github.com/toransahu/grpc-eg-go/utils"
 	"github.com/toransahu/grpc-eg-go/utils/stack"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -26,6 +28,7 @@ const (
 	SUB               = "SUB"
 	MUL               = "MUL"
 	DIV               = "DIV"
+	FIB               = "FIB"
 )
 
 type MachineServer struct{}
@@ -83,5 +86,40 @@ func (s *MachineServer) Execute(ctx context.Context, instructions *machine.Instr
 
 // ServerStreamingExecute runs the set of instructions given and streams a sequence of Results.
 func (s *MachineServer) ServerStreamingExecute(instructions *machine.InstructionSet, stream machine.Machine_ServerStreamingExecuteServer) error {
-	return status.Error(codes.Unimplemented, "ServerStreamingExecute() not implemented yet")
+	if len(instructions.GetInstructions()) == 0 {
+		return status.Error(codes.InvalidArgument, "No valid instructions received")
+	}
+
+	var stack stack.Stack
+
+	for _, instruction := range instructions.GetInstructions() {
+		operand := instruction.GetOperand()
+		operator := instruction.GetOperator()
+		op_type := OperatorType(operator)
+
+		log.Printf("Operand: %v, Operator: %v\n", operand, operator)
+
+		switch op_type {
+		case PUSH:
+			stack.Push(float32(operand))
+		case POP:
+			stack.Pop()
+		case FIB:
+			n, popped := stack.Pop()
+
+			if !popped {
+				return status.Error(codes.Aborted, "Invalid sets of instructions. Execution aborted")
+			}
+
+			if op_type == FIB {
+				for f := range utils.FibonacciRange(int(n)) {
+					log.Println(float32(f))
+					stream.Send(&machine.Result{Output: float32(f)})
+				}
+			}
+		default:
+			return status.Errorf(codes.Unimplemented, "Operation '%s' not implemented yet", operator)
+		}
+	}
+	return nil
 }
