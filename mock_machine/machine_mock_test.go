b06988aa4f120a0f85ac514739603585358ead7a
Toran Sahu <toran.sahu@yahoo.com>
2021-02-13 17:08:27 +0530

    add unittest to test client-side logic for server-side streaming using mock

--- mock_machine/machine_mock_test.go
+++ mock_machine/machine_mock_test.go
@@ -9,6 +9,7 @@ package mock_machine_test
 
 import (
 	context "context"
+	"fmt"
 	"log"
 	"testing"
 	"time"
@@ -57,4 +58,47 @@ func TestExecute(t *testing.T) {
 	).Return(&machine.Result{Output: 30}, nil)
 
 	testExecute(t, mockMachineClient)
+}
+
+func testServerStreamingExecute(t *testing.T, mockMachineClient machine.MachineClient, instructionSet *machine.InstructionSet) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	stream, err := mockMachineClient.ServerStreamingExecute(ctx, instructionSet)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	result, err := stream.Recv()
+	if err != nil {
+		return err
+	}
+	got := result.GetOutput()
+	want := float32(0)
+	if got != want {
+		return fmt.Errorf("stream.Recv() = %v, want %v", got, want)
+	}
+	return nil
+}
+
+func TestServerStreamingExecute(t *testing.T) {
+	instructions := []*machine.Instruction{}
+	instructions = append(instructions, &machine.Instruction{Operand: 1, Operator: "PUSH"})
+	instructions = append(instructions, &machine.Instruction{Operator: "FIB"})
+	instructionSet := &machine.InstructionSet{Instructions: instructions}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockMachineClient := mock_machine.NewMockMachineClient(ctrl)
+	clientStream := mock_machine.NewMockMachine_ServerStreamingExecuteClient(ctrl)
+
+	clientStream.EXPECT().Recv().Return(&machine.Result{Output: 0}, nil)
+
+	mockMachineClient.EXPECT().ServerStreamingExecute(
+		gomock.Any(),   // context
+		instructionSet, // rpc uniary message
+	).Return(clientStream, nil)
+
+	if err := testServerStreamingExecute(t, mockMachineClient, instructionSet); err != nil {
+		t.Fatalf("Test failed: %v", err)
+	}
 }
