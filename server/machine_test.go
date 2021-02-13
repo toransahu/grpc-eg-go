668e9699059d4039c22e6e6bf93f02ea3bb074b3
Toran Sahu <toran.sahu@yahoo.com>
2021-02-13 17:04:31 +0530

    add unit test for server-side streaming

--- server/machine_test.go
+++ server/machine_test.go
@@ -11,7 +11,9 @@ import (
 	"context"
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/toransahu/grpc-eg-go/machine"
+	"github.com/toransahu/grpc-eg-go/mock_machine"
 )
 
 func TestExecute(t *testing.T) {
@@ -50,6 +52,57 @@ func TestExecute(t *testing.T) {
 		}
 		if resp.Output != tt.want {
 			t.Errorf("got Execute(%v)=%v, wanted %v", tt.instructions, resp.Output, tt.want)
+		}
+	}
+}
+
+func TestServerStreamingExecute(t *testing.T) {
+	s := MachineServer{}
+
+	// set up test table
+	tests := []struct {
+		instructions []*machine.Instruction
+		want         []float32
+	}{
+		{
+			instructions: []*machine.Instruction{
+				{Operand: 5, Operator: "PUSH"},
+				{Operator: "FIB"},
+			},
+			want: []float32{0, 1, 1, 2, 3, 5},
+		},
+		{
+			instructions: []*machine.Instruction{
+				{Operand: 6, Operator: "PUSH"},
+				{Operator: "FIB"},
+			},
+			want: []float32{0, 1, 1, 2, 3, 5, 8},
+		},
+	}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockServerStream := mock_machine.NewMockMachine_ServerStreamingExecuteServer(ctrl)
+	for _, tt := range tests {
+		mockResults := []*machine.Result{}
+		mockServerStream.EXPECT().Send(gomock.Any()).DoAndReturn(
+			func(result *machine.Result) error {
+				mockResults = append(mockResults, result)
+				return nil
+			}).AnyTimes()
+
+		req := &machine.InstructionSet{Instructions: tt.instructions}
+
+		err := s.ServerStreamingExecute(req, mockServerStream)
+		if err != nil {
+			t.Errorf("ServerStreamingExecute(%v) got unexpected error: %v", req, err)
+		}
+		for i, result := range mockResults {
+			got := result.GetOutput()
+			want := tt.want[i]
+			if got != want {
+				t.Errorf("got %v, want %v", got, want)
+			}
 		}
 	}
 }
