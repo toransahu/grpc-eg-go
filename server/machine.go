7321c19c340ed1396b88a44378fc3ea86e089d4b
Toran Sahu <toran.sahu@yahoo.com>
2021-02-14 20:16:43 +0530

    update the server code to make Execute() a bi-directional (server + client-side streaming) RPC - so that the client can stream the instructions rather than sending a set of intructions to the server & the server can respond with stream of results - getting rid of InstructionSet, ServerStreamingExecute()

--- server/machine.go
+++ server/machine.go
@@ -40,17 +40,6 @@ func (s *MachineServer) Execute(stream machine.Machine_ExecuteServer) error {
 		instruction, err := stream.Recv()
 		if err == io.EOF {
 			log.Println("EOF")
-			output, popped := stack.Pop()
-			if !popped {
-				return status.Error(codes.Aborted, "Invalid sets of instructions. Execution aborted")
-			}
-
-			if err := stream.SendAndClose(&machine.Result{
-				Output: output,
-			}); err != nil {
-				return err
-			}
-
 			return nil
 		}
 		if err != nil {
@@ -76,43 +65,21 @@ func (s *MachineServer) Execute(stream machine.Machine_ExecuteServer) error {
 				return status.Error(codes.Aborted, "Invalid sets of instructions. Execution aborted")
 			}
 
+			var res float32
 			if op_type == ADD {
-				stack.Push(item1 + item2)
+				res = item1 + item2
 			} else if op_type == SUB {
-				stack.Push(item1 - item2)
+				res = item1 - item2
 			} else if op_type == MUL {
-				stack.Push(item1 * item2)
+				res = item1 * item2
 			} else if op_type == DIV {
-				stack.Push(item1 / item2)
+				res = item1 / item2
 			}
 
-		default:
-			return status.Errorf(codes.Unimplemented, "Operation '%s' not implemented yet", operator)
-		}
-
-	}
-}
-
-// ServerStreamingExecute runs the set of instructions given and streams a sequence of Results.
-func (s *MachineServer) ServerStreamingExecute(instructions *machine.InstructionSet, stream machine.Machine_ServerStreamingExecuteServer) error {
-	if len(instructions.GetInstructions()) == 0 {
-		return status.Error(codes.InvalidArgument, "No valid instructions received")
-	}
-
-	var stack stack.Stack
-
-	for _, instruction := range instructions.GetInstructions() {
-		operand := instruction.GetOperand()
-		operator := instruction.GetOperator()
-		op_type := OperatorType(operator)
-
-		log.Printf("Operand: %v, Operator: %v\n", operand, operator)
-
-		switch op_type {
-		case PUSH:
-			stack.Push(float32(operand))
-		case POP:
-			stack.Pop()
+			stack.Push(res)
+			if err := stream.Send(&machine.Result{Output: float32(res)}); err != nil {
+				return err
+			}
 		case FIB:
 			n, popped := stack.Pop()
 
@@ -122,13 +89,14 @@ func (s *MachineServer) ServerStreamingExecute(instructions *machine.Instruction
 
 			if op_type == FIB {
 				for f := range utils.FibonacciRange(int(n)) {
-					log.Println(float32(f))
-					stream.Send(&machine.Result{Output: float32(f)})
+					if err := stream.Send(&machine.Result{Output: float32(f)}); err != nil {
+						return err
+					}
 				}
 			}
 		default:
 			return status.Errorf(codes.Unimplemented, "Operation '%s' not implemented yet", operator)
 		}
+
 	}
-	return nil
 }
