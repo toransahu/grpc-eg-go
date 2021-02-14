ed0fa3c88a8cd6cb70b28835c3dd430912711fe2
Toran Sahu <toran.sahu@yahoo.com>
2021-02-14 20:14:16 +0530

    generate updated golang code from machine.proto

--- machine/machine.pb.go
+++ machine/machine.pb.go
@@ -71,45 +71,6 @@ func (m *Instruction) GetOperand() int32 {
 	return 0
 }
 
-type InstructionSet struct {
-	Instructions         []*Instruction `protobuf:"bytes,1,rep,name=instructions,proto3" json:"instructions,omitempty"`
-	XXX_NoUnkeyedLiteral struct{}       `json:"-"`
-	XXX_unrecognized     []byte         `json:"-"`
-	XXX_sizecache        int32          `json:"-"`
-}
-
-func (m *InstructionSet) Reset()         { *m = InstructionSet{} }
-func (m *InstructionSet) String() string { return proto.CompactTextString(m) }
-func (*InstructionSet) ProtoMessage()    {}
-func (*InstructionSet) Descriptor() ([]byte, []int) {
-	return fileDescriptor_84b4f59d98cc997c, []int{1}
-}
-
-func (m *InstructionSet) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_InstructionSet.Unmarshal(m, b)
-}
-func (m *InstructionSet) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_InstructionSet.Marshal(b, m, deterministic)
-}
-func (m *InstructionSet) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_InstructionSet.Merge(m, src)
-}
-func (m *InstructionSet) XXX_Size() int {
-	return xxx_messageInfo_InstructionSet.Size(m)
-}
-func (m *InstructionSet) XXX_DiscardUnknown() {
-	xxx_messageInfo_InstructionSet.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_InstructionSet proto.InternalMessageInfo
-
-func (m *InstructionSet) GetInstructions() []*Instruction {
-	if m != nil {
-		return m.Instructions
-	}
-	return nil
-}
-
 type Result struct {
 	Output               float32  `protobuf:"fixed32,1,opt,name=output,proto3" json:"output,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
@@ -121,7 +82,7 @@ func (m *Result) Reset()         { *m = Result{} }
 func (m *Result) String() string { return proto.CompactTextString(m) }
 func (*Result) ProtoMessage()    {}
 func (*Result) Descriptor() ([]byte, []int) {
-	return fileDescriptor_84b4f59d98cc997c, []int{2}
+	return fileDescriptor_84b4f59d98cc997c, []int{1}
 }
 
 func (m *Result) XXX_Unmarshal(b []byte) error {
@@ -151,7 +112,6 @@ func (m *Result) GetOutput() float32 {
 
 func init() {
 	proto.RegisterType((*Instruction)(nil), "machine.Instruction")
-	proto.RegisterType((*InstructionSet)(nil), "machine.InstructionSet")
 	proto.RegisterType((*Result)(nil), "machine.Result")
 }
 
@@ -160,22 +120,18 @@ func init() {
 }
 
 var fileDescriptor_84b4f59d98cc997c = []byte{
-	// 225 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x90, 0xc1, 0x4a, 0xc4, 0x30,
-	0x10, 0x86, 0xcd, 0x8a, 0xad, 0xce, 0x8a, 0xc2, 0xa0, 0x6b, 0xd8, 0x53, 0xc8, 0x29, 0xa7, 0x55,
-	0x56, 0x0f, 0xde, 0x45, 0x41, 0xc1, 0x4b, 0xf2, 0x04, 0xb5, 0x0e, 0x1a, 0x70, 0x93, 0x92, 0x4e,
-	0xc4, 0x37, 0xf0, 0xb5, 0x85, 0x6e, 0xbb, 0x76, 0xa1, 0xa7, 0xe4, 0xfb, 0xf9, 0xf9, 0xf8, 0x19,
-	0xb8, 0xdc, 0x54, 0xf5, 0xa7, 0x0f, 0x74, 0xdd, 0xbf, 0xab, 0x26, 0x45, 0x8e, 0x58, 0xf6, 0xa8,
-	0x1f, 0x60, 0xfe, 0x1c, 0x5a, 0x4e, 0xb9, 0x66, 0x1f, 0x03, 0x2e, 0xe1, 0x38, 0x36, 0x94, 0x2a,
-	0x8e, 0x49, 0x0a, 0x25, 0xcc, 0x89, 0xdd, 0x31, 0x4a, 0x28, 0xbb, 0x7f, 0x78, 0x97, 0x33, 0x25,
-	0xcc, 0x91, 0x1d, 0x50, 0xbf, 0xc0, 0xd9, 0x48, 0xe2, 0x88, 0xf1, 0x1e, 0x4e, 0xfd, 0x7f, 0xd2,
-	0x4a, 0xa1, 0x0e, 0xcd, 0x7c, 0x7d, 0xb1, 0x1a, 0x56, 0x8c, 0xea, 0x76, 0xaf, 0xa9, 0x15, 0x14,
-	0x96, 0xda, 0xfc, 0xc5, 0xb8, 0x80, 0x22, 0x66, 0x6e, 0x32, 0x77, 0x4b, 0x66, 0xb6, 0xa7, 0xf5,
-	0xaf, 0x80, 0xf2, 0x75, 0xeb, 0xc1, 0x3b, 0x28, 0x1f, 0x7f, 0xa8, 0xce, 0x4c, 0x38, 0x29, 0x5f,
-	0x9e, 0xef, 0xd2, 0xad, 0x55, 0x1f, 0x18, 0x81, 0x4f, 0xb0, 0x70, 0x94, 0xbe, 0x29, 0x39, 0x4e,
-	0x54, 0x6d, 0x7c, 0xf8, 0x18, 0x24, 0x57, 0x53, 0x12, 0x47, 0x3c, 0xe1, 0xb9, 0x11, 0x6f, 0x45,
-	0x77, 0xcc, 0xdb, 0xbf, 0x00, 0x00, 0x00, 0xff, 0xff, 0x48, 0xbc, 0x16, 0xfa, 0x65, 0x01, 0x00,
-	0x00,
+	// 166 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0xcd, 0x4d, 0x4c, 0xce,
+	0xc8, 0xcc, 0x4b, 0xd5, 0x87, 0xd2, 0x7a, 0x05, 0x45, 0xf9, 0x25, 0xf9, 0x42, 0xec, 0x50, 0xae,
+	0x92, 0x33, 0x17, 0xb7, 0x67, 0x5e, 0x71, 0x49, 0x51, 0x69, 0x72, 0x49, 0x66, 0x7e, 0x9e, 0x90,
+	0x14, 0x17, 0x47, 0x7e, 0x41, 0x6a, 0x51, 0x62, 0x49, 0x7e, 0x91, 0x04, 0xa3, 0x02, 0xa3, 0x06,
+	0x67, 0x10, 0x9c, 0x2f, 0x24, 0xc1, 0xc5, 0x0e, 0x66, 0xe7, 0xa5, 0x48, 0x30, 0x29, 0x30, 0x6a,
+	0xb0, 0x06, 0xc1, 0xb8, 0x4a, 0x0a, 0x5c, 0x6c, 0x41, 0xa9, 0xc5, 0xa5, 0x39, 0x25, 0x42, 0x62,
+	0x5c, 0x6c, 0xf9, 0xa5, 0x25, 0x05, 0xa5, 0x25, 0x60, 0xdd, 0x4c, 0x41, 0x50, 0x9e, 0x91, 0x23,
+	0x17, 0xbb, 0x2f, 0xc4, 0x46, 0x21, 0x33, 0x2e, 0x76, 0xd7, 0x8a, 0xd4, 0xe4, 0xd2, 0x92, 0x54,
+	0x21, 0x11, 0x3d, 0x98, 0xab, 0x90, 0xdc, 0x20, 0xc5, 0x0f, 0x17, 0x85, 0x18, 0xaa, 0xc4, 0xa0,
+	0xc1, 0x68, 0xc0, 0x98, 0xc4, 0x06, 0x76, 0xb9, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0x23, 0xc6,
+	0x6c, 0xfd, 0xd2, 0x00, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -191,7 +147,6 @@ const _ = grpc.SupportPackageIsVersion6
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type MachineClient interface {
 	Execute(ctx context.Context, opts ...grpc.CallOption) (Machine_ExecuteClient, error)
-	ServerStreamingExecute(ctx context.Context, in *InstructionSet, opts ...grpc.CallOption) (Machine_ServerStreamingExecuteClient, error)
 }
 
 type machineClient struct {
@@ -213,7 +168,7 @@ func (c *machineClient) Execute(ctx context.Context, opts ...grpc.CallOption) (M
 
 type Machine_ExecuteClient interface {
 	Send(*Instruction) error
-	CloseAndRecv() (*Result, error)
+	Recv() (*Result, error)
 	grpc.ClientStream
 }
 
@@ -225,42 +180,7 @@ func (x *machineExecuteClient) Send(m *Instruction) error {
 	return x.ClientStream.SendMsg(m)
 }
 
-func (x *machineExecuteClient) CloseAndRecv() (*Result, error) {
-	if err := x.ClientStream.CloseSend(); err != nil {
-		return nil, err
-	}
-	m := new(Result)
-	if err := x.ClientStream.RecvMsg(m); err != nil {
-		return nil, err
-	}
-	return m, nil
-}
-
-func (c *machineClient) ServerStreamingExecute(ctx context.Context, in *InstructionSet, opts ...grpc.CallOption) (Machine_ServerStreamingExecuteClient, error) {
-	stream, err := c.cc.NewStream(ctx, &_Machine_serviceDesc.Streams[1], "/machine.Machine/ServerStreamingExecute", opts...)
-	if err != nil {
-		return nil, err
-	}
-	x := &machineServerStreamingExecuteClient{stream}
-	if err := x.ClientStream.SendMsg(in); err != nil {
-		return nil, err
-	}
-	if err := x.ClientStream.CloseSend(); err != nil {
-		return nil, err
-	}
-	return x, nil
-}
-
-type Machine_ServerStreamingExecuteClient interface {
-	Recv() (*Result, error)
-	grpc.ClientStream
-}
-
-type machineServerStreamingExecuteClient struct {
-	grpc.ClientStream
-}
-
-func (x *machineServerStreamingExecuteClient) Recv() (*Result, error) {
+func (x *machineExecuteClient) Recv() (*Result, error) {
 	m := new(Result)
 	if err := x.ClientStream.RecvMsg(m); err != nil {
 		return nil, err
@@ -271,7 +191,6 @@ func (x *machineServerStreamingExecuteClient) Recv() (*Result, error) {
 // MachineServer is the server API for Machine service.
 type MachineServer interface {
 	Execute(Machine_ExecuteServer) error
-	ServerStreamingExecute(*InstructionSet, Machine_ServerStreamingExecuteServer) error
 }
 
 // UnimplementedMachineServer can be embedded to have forward compatible implementations.
@@ -280,9 +199,6 @@ type UnimplementedMachineServer struct {
 
 func (*UnimplementedMachineServer) Execute(srv Machine_ExecuteServer) error {
 	return status.Errorf(codes.Unimplemented, "method Execute not implemented")
-}
-func (*UnimplementedMachineServer) ServerStreamingExecute(req *InstructionSet, srv Machine_ServerStreamingExecuteServer) error {
-	return status.Errorf(codes.Unimplemented, "method ServerStreamingExecute not implemented")
 }
 
 func RegisterMachineServer(s *grpc.Server, srv MachineServer) {
@@ -294,7 +210,7 @@ func _Machine_Execute_Handler(srv interface{}, stream grpc.ServerStream) error {
 }
 
 type Machine_ExecuteServer interface {
-	SendAndClose(*Result) error
+	Send(*Result) error
 	Recv() (*Instruction, error)
 	grpc.ServerStream
 }
@@ -303,7 +219,7 @@ type machineExecuteServer struct {
 	grpc.ServerStream
 }
 
-func (x *machineExecuteServer) SendAndClose(m *Result) error {
+func (x *machineExecuteServer) Send(m *Result) error {
 	return x.ServerStream.SendMsg(m)
 }
 
@@ -315,27 +231,6 @@ func (x *machineExecuteServer) Recv() (*Instruction, error) {
 	return m, nil
 }
 
-func _Machine_ServerStreamingExecute_Handler(srv interface{}, stream grpc.ServerStream) error {
-	m := new(InstructionSet)
-	if err := stream.RecvMsg(m); err != nil {
-		return err
-	}
-	return srv.(MachineServer).ServerStreamingExecute(m, &machineServerStreamingExecuteServer{stream})
-}
-
-type Machine_ServerStreamingExecuteServer interface {
-	Send(*Result) error
-	grpc.ServerStream
-}
-
-type machineServerStreamingExecuteServer struct {
-	grpc.ServerStream
-}
-
-func (x *machineServerStreamingExecuteServer) Send(m *Result) error {
-	return x.ServerStream.SendMsg(m)
-}
-
 var _Machine_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "machine.Machine",
 	HandlerType: (*MachineServer)(nil),
@@ -344,12 +239,8 @@ var _Machine_serviceDesc = grpc.ServiceDesc{
 		{
 			StreamName:    "Execute",
 			Handler:       _Machine_Execute_Handler,
-			ClientStreams: true,
-		},
-		{
-			StreamName:    "ServerStreamingExecute",
-			Handler:       _Machine_ServerStreamingExecute_Handler,
 			ServerStreams: true,
+			ClientStreams: true,
 		},
 	},
 	Metadata: "machine/machine.proto",
